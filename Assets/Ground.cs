6dffb32a9042ad454c6348f79aa2d48d097c3ef1
Schwelle76 <mattes.zens@web.de>
2022-11-25 16:55:25 +0100

    added grassground

---
+++ Assets/Ground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newZ = player.position.z + transform.localScale.z / 2.1f;
+     transform.position = new Vector3 (transform.position.x, transform.position.y, newZ); 
+    }
+}
